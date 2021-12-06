dd91b7384e281946166d5c32307805ccdc0d59e8
Bence Kaszás <bence.kaszas@gmail.com>
2021-12-06 11:03:27 +0000

    Day 6 complete

---
+++ Day06.hs
@@ -0,0 +1,15 @@
+module Day06 where
+
+import Data.List.Split (splitOn)
+
+grow day xs = (map sum . iterate nextDay . dayCount $ xs) !! day
+  where
+    dayCount xs = [length $ filter (== daysLeft) xs | daysLeft <- [0..8]]
+    nextDay [zero,one,two,three,four,five,six,seven,eight] = [one, two, three, four, five, six, seven + zero, eight, zero]
+
+main :: IO ()
+main = do
+    inps <- readFile "inputs/06.txt"
+    let xs = map read $ splitOn "," inps :: [Int]
+    print ("Part 1: " ++ show (grow 80 xs))
+    print ("Part 2: " ++ show (grow 256 xs))
